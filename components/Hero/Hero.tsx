cd6cd108178ad28a8f81efd7003571491ca136c6
Mariel Martinez <mcortez.martinez@gmail.com>
2020-10-03 17:35:58 -0400

    Add link to about-me

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-unescaped-entities */
 import React, { ReactElement } from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
@@ -15,10 +14,10 @@ const Hero = () : ReactElement => {
           <Col sm={9}>
           <h1>Full Stack Web Developer</h1>
             <p>
-              I'm a full stack web developer with industry experience in start-up environments. I have a background in sales that gives me a unique perspective as a developer, and drives me to make changes that matter to the customer.
+              I&apos;m a full stack web developer with industry experience in start-up environments. I have a background in sales that gives me a unique perspective as a developer, and drives me to make changes that matter to the customer.
             </p>
             <p>
-              <Button variant="primary">Learn more</Button>
+              <Button variant="primary"  href="about-me">Learn more</Button>
             </p>
           </Col>
           <Col sm={3}>
