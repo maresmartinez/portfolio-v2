df7f29ac82a24866af85b798259a534dec48f874
Mariel Martinez <mcortez.martinez@gmail.com>
2020-10-03 14:27:47 -0400

    Comment out unused variable for now

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,6 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import { /*NextApiRequest,*/ NextApiResponse } from 'next'
 
-export default (req: NextApiRequest, res: NextApiResponse) : void => {
+export default (/* req: NextApiRequest,*/ res: NextApiResponse) : void => {
   res.statusCode = 200
   res.json({ name: 'John Doe' })
 }
