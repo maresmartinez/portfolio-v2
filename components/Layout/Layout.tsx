32f33a32338cb7d4968732d599f5d14585b9da25
Mariel Martinez <mcortez.martinez@gmail.com>
2020-10-04 20:09:21 -0400

    Add all technical writing reports to navbar

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement } from 'react';
 import Head from 'next/head';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import styles from './Layout.module.css';
 import cs from 'classnames';
 
@@ -22,7 +23,14 @@ const Layout = ({ children }: { children: ReactElement }): ReactElement => {
               <Nav.Link href="/">Home</Nav.Link>
               <Nav.Link href="/about-me">About</Nav.Link>
               <Nav.Link href="/projects">Projects</Nav.Link>
-              <Nav.Link href="/technical-writing">Technical Writing</Nav.Link>
+              <NavDropdown title="Technical Writing" id="technical-writing-dropdown">
+                <NavDropdown.Item href="/technical-writing">Overview</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/technical-writing/raspberripi-web-server">Create a Raspberry Pi Web Server</NavDropdown.Item>
+                <NavDropdown.Item href="/technical-writing/setup-adds">Setting Up An Active Directory Domain</NavDropdown.Item>
+                <NavDropdown.Item href="/technical-writing/using-wds">Using Windows Deployment Services</NavDropdown.Item>
+                <NavDropdown.Item href="/technical-writing/adds-forest">Active Directory Forest with Multiple Servers</NavDropdown.Item>
+              </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
